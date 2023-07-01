4b26e01a9d085b5200ece1177bd5e385ceaf7d7b
xDKL1 <david_lewis4@outlook.com>
2023-07-01 21:41:45 +0100

    Old version submit
    This is the old version of the calculator system for future
    reference.

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-function add(numbers){
-    let sum = numbers.reduce((x,y) => {
-        return x+y;
-    });
+function add(n1, n2){
+    let sum = n1+n2;
     return sum;
 }
 
 
-function subtract(numbers){
-    let sum = numbers.reduce((x,y) => {
-        return x-y;
-    });
+function subtract(n1,n2){
+    let difference = n1-n2;
+    return difference
+}
+
+function multiply(n1,n2){
+    let sum = n1*n2;
     return sum;
 }
 
-function multiply(numbers){
-    let sum = numbers.reduce((x,y) => {
-        return x*y;
-    });
-    return sum;
-}
-
-function divide(numbers){
-    let sum = numbers.reduce((x,y) => {
-        return x/y;
-    });
+function divide(n1,n2){
+    let sum = n1/n2;
     return sum;
 }
 
@@ -31,3 +23,77 @@ function divide(numbers){
 //console.log(subtract([2,1]));
 //console.log(multiply([2,1]));
 //console.log(divide([5,2]));
+let number1, number2, operator;
+
+function operate(n1,n2, operator){
+    if (operator == 'plus') {
+        return add(n1,n2);
+    } else if (operator == 'minus'){
+        return subtract(n1,n2);
+    } else if (operator == 'multiply'){
+        return multiply(n1,n2);
+    } else if (operator == 'divide'){
+        return divide(n1,n2);
+    }
+}
+
+const number = document.querySelectorAll('.display-change');
+const display = document.querySelector('.display');
+const operator_class = document.querySelectorAll('.operator'); 
+let displayValue = '';
+
+let displayChange = (event) =>{
+    const clickedNumber = event.target.textContent;
+    if (clickedNumber == 'C'){
+        display.textContent = '';
+        displayValue = '';
+        return;
+    } else if (clickedNumber == 'AC'){
+        display.textContent = '';
+        displayValue = '';
+        return;
+    } else if (clickedNumber == '.'){
+        return;
+    }
+   // console.log('number');
+    displayValue += clickedNumber;
+    display.textContent = displayValue;
+    //console.log(displayValue);
+}
+let counter = 1;
+let result;
+let numberSubmit = (event) =>{
+    selected_operator = event.target.textContent;
+
+    if (counter == 1) {
+        number1 = parseInt(displayValue);
+        displayValue = '';
+        display.textContent = '';
+        counter++;
+    } else if (counter == 2){
+        number2 = parseInt(displayValue);
+        displayValue = '';
+        display.textContent = '';
+        if (selected_operator == '+'){
+            result = add(number1,number2);
+            number1 = result;
+            console.log(result);
+        } else if (selected_operator == '-'){
+            result = subtract(number1,number2);
+            number1 = result;
+            console.log(result);
+        } else if (selected_operator == '='){
+            console.log(result);
+        }
+    }
+    
+}
+
+
+number.forEach((item) =>{
+    item.addEventListener('click', displayChange);
+})
+
+operator_class.forEach((item) =>{
+    item.addEventListener('click', numberSubmit)
+})
